cf1f6b7d2027cfa2e6fdce1e54916b4cd8aa55de
Oscar Allstar <oscar96261@gmail.com>
2021-04-23 16:25:04 -0500

    Proyecto Calculadora simple completo

---
+++ Ejemplo de Calculadora simple/Ejemplo de Calculadora simple/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Ejemplo_de_Calculadora_simple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtNum1 = new System.Windows.Forms.TextBox();
+            this.TxtNum2 = new System.Windows.Forms.TextBox();
+            this.BtnSuma = new System.Windows.Forms.Button();
+            this.BtnResta = new System.Windows.Forms.Button();
+            this.BtnMultiplicacion = new System.Windows.Forms.Button();
+            this.BtnDivicion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese un numero:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese un numero:";
+            // 
+            // TxtNum1
+            // 
+            this.TxtNum1.Location = new System.Drawing.Point(27, 45);
+            this.TxtNum1.Name = "TxtNum1";
+            this.TxtNum1.Size = new System.Drawing.Size(218, 20);
+            this.TxtNum1.TabIndex = 2;
+            // 
+            // TxtNum2
+            // 
+            this.TxtNum2.Location = new System.Drawing.Point(27, 84);
+            this.TxtNum2.Name = "TxtNum2";
+            this.TxtNum2.Size = new System.Drawing.Size(218, 20);
+            this.TxtNum2.TabIndex = 3;
+            // 
+            // BtnSuma
+            // 
+            this.BtnSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSuma.Location = new System.Drawing.Point(30, 125);
+            this.BtnSuma.Name = "BtnSuma";
+            this.BtnSuma.Size = new System.Drawing.Size(96, 27);
+            this.BtnSuma.TabIndex = 4;
+            this.BtnSuma.Text = "+";
+            this.BtnSuma.UseVisualStyleBackColor = true;
+            this.BtnSuma.Click += new System.EventHandler(this.BtnSuma_Click);
+            // 
+            // BtnResta
+            // 
+            this.BtnResta.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnResta.Location = new System.Drawing.Point(149, 125);
+            this.BtnResta.Name = "BtnResta";
+            this.BtnResta.Size = new System.Drawing.Size(96, 27);
+            this.BtnResta.TabIndex = 5;
+            this.BtnResta.Text = "-";
+            this.BtnResta.UseVisualStyleBackColor = true;
+            this.BtnResta.Click += new System.EventHandler(this.BtnResta_Click);
+            // 
+            // BtnMultiplicacion
+            // 
+            this.BtnMultiplicacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnMultiplicacion.Location = new System.Drawing.Point(30, 175);
+            this.BtnMultiplicacion.Name = "BtnMultiplicacion";
+            this.BtnMultiplicacion.Size = new System.Drawing.Size(96, 27);
+            this.BtnMultiplicacion.TabIndex = 6;
+            this.BtnMultiplicacion.Text = "*";
+            this.BtnMultiplicacion.UseVisualStyleBackColor = true;
+            this.BtnMultiplicacion.Click += new System.EventHandler(this.BtnMultiplicacion_Click);
+            // 
+            // BtnDivicion
+            // 
+            this.BtnDivicion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDivicion.Location = new System.Drawing.Point(149, 175);
+            this.BtnDivicion.Name = "BtnDivicion";
+            this.BtnDivicion.Size = new System.Drawing.Size(96, 27);
+            this.BtnDivicion.TabIndex = 7;
+            this.BtnDivicion.Text = "/";
+            this.BtnDivicion.UseVisualStyleBackColor = true;
+            this.BtnDivicion.Click += new System.EventHandler(this.BtnDivicion_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.BtnDivicion);
+            this.Controls.Add(this.BtnMultiplicacion);
+            this.Controls.Add(this.BtnResta);
+            this.Controls.Add(this.BtnSuma);
+            this.Controls.Add(this.TxtNum2);
+            this.Controls.Add(this.TxtNum1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtNum1;
+        private System.Windows.Forms.TextBox TxtNum2;
+        private System.Windows.Forms.Button BtnSuma;
+        private System.Windows.Forms.Button BtnResta;
+        private System.Windows.Forms.Button BtnMultiplicacion;
+        private System.Windows.Forms.Button BtnDivicion;
+    }
+}
+
---
+++ Ejemplo de Calculadora simple/Ejemplo de Calculadora simple/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo_de_Calculadora_simple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSuma_Click(object sender, EventArgs e)
+        {
+            Double Num1 = 0;
+            Double Num2 = 0;
+            Double Operacion = 0;
+
+            Num1 = Double.Parse(TxtNum1.Text);
+            Num2 = Double.Parse(TxtNum2.Text);
+
+            Operacion = Num1 + Num2;
+
+            MessageBox.Show("El resultado es " + Operacion);
+        }
+
+        private void BtnResta_Click(object sender, EventArgs e)
+        {
+            Double Num1 = 0;
+            Double Num2 = 0;
+            Double Operacion = 0;
+
+            Num1 = Double.Parse(TxtNum1.Text);
+            Num2 = Double.Parse(TxtNum2.Text);
+
+            Operacion = Num1 - Num2;
+
+            MessageBox.Show("El resultado es " + Operacion);
+        }
+
+        private void BtnMultiplicacion_Click(object sender, EventArgs e)
+        {
+            Double Num1 = 0;
+            Double Num2 = 0;
+            Double Operacion = 0;
+
+            Num1 = Double.Parse(TxtNum1.Text);
+            Num2 = Double.Parse(TxtNum2.Text);
+
+            Operacion = Num1 * Num2;
+
+            MessageBox.Show("El resultado es " + Operacion);
+        }
+
+        private void BtnDivicion_Click(object sender, EventArgs e)
+        {
+            Double Num1 = 0;
+            Double Num2 = 0;
+            Double Operacion = 0;
+
+            Num1 = Double.Parse(TxtNum1.Text);
+            Num2 = Double.Parse(TxtNum2.Text);
+
+            Operacion = Num1 / Num2;
+
+            MessageBox.Show("El resultado es " + Operacion);
+        }
+    }
+}
